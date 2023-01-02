0a2a1362184e9a2e20502cfc5e1a0572cb2776d4
Pham Chu Duong <pduong244@gmail.com>
2023-01-02 09:22:27 +0700

    sidebar admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,20 @@
+import SideBar from "./SideBar";
+import { FaBars } from "react-icons/fa";
+import './Admin.scss';
+import { useState } from "react";
+
 function Admin() {
+
+    const [collapsed, setCollapsed] = useState(false);
+
     return (
-        <div>
-            Admin
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+            </div>
         </div>
     );
 }
