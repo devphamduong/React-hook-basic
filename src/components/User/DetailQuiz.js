9e5404f57f93785fdbf61704d7b9492a7db6f046
Pham Chu Duong <pduong244@gmail.com>
2023-01-04 13:36:16 +0700

    Process Data - Xử Lý Data Phía Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDataQuiz } from '../../services/apiServices';
@@ -13,7 +14,27 @@ function DetailQuiz(props) {
     }, [quizId]);
 
     const getQuestions = async () => {
-        let data = await getDataQuiz(quizId);
+        let res = await getDataQuiz(quizId);
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `id` property
+                .groupBy("id")
+                // `key` is group's name (id), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    });
+                    return { questionId: key, answers, questionDescription, image };
+                })
+                .value();
+        }
     };
 
     const formatedId = (id) => {
