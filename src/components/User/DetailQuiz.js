dacc85ad890f32dfc80ce3afb105772255735763
Pham Chu Duong <pduong244@gmail.com>
2023-01-04 14:00:51 +0700

    Design Quiz Layout - Tạo Base Giao Diện Bài Thi

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { getDataQuiz } from '../../services/apiServices';
+import './DetailQuiz.scss';
 
 function DetailQuiz(props) {
 
     const params = useParams();
+
+    const formatedId = (id) => {
+        return id.replace(/[^a-zA-Z0-9]/g, '');
+    };
     const quizId = formatedId(params.id);
+    const location = useLocation();
     const [listQuestion, setListQuestion] = useState([]);
 
     useEffect(() => {
@@ -37,13 +43,33 @@ function DetailQuiz(props) {
         }
     };
 
-    const formatedId = (id) => {
-        return id.replace(/[^a-zA-Z0-9]/g, '');
-    };
-
     return (
-        <div>
-            abc
+        <div className='detail-quiz-container'>
+            <div className='left'>
+                <div className='title'>
+                    Quiz {quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr></hr>
+                <div className='question-image'>
+                    <img />
+                </div>
+                <div className='question-content'>
+                    <div className='question'>
+                        Question 1:  ?
+                    </div>
+                    <div className='answers'>
+                        <div className='answer-child'>A. </div>
+                        <div className='answer-child'>B. </div>
+                    </div>
+                </div>
+                <div className='question-footer'>
+                    <button className='btn btn-secondary'>Prev</button>
+                    <button className='btn btn-primary'>Next</button>
+                </div>
+            </div>
+            <div className='right'>
+                count down
+            </div>
         </div>
     );
 }
