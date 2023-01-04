37c524a2a7bfa4c98c0524cd664be3dcf3eef06c
Pham Chu Duong <pduong244@gmail.com>
2023-01-04 09:00:10 +0700

    Loading Bar - Hiển Thị Thanh Loading Khi Gọi APIs

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { login } from '../../services/apiServices';
 import { doLogin } from '../../redux/action/userAction';
+import { ImSpinner10 } from 'react-icons/im';
 import './Login.scss';
 
 function Login(props) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -23,13 +25,16 @@ function Login(props) {
             toast.error("Invalid password!");
             return;
         }
+        setIsLoading(true);
         let data = await login(email, password);
         if (data && data.EC === 0) {
             dispatch(doLogin(data));
             toast.success(data.EM);
+            setIsLoading(false);
             navigate('/');
         } else if (data && data.EC !== 0) {
             toast.error(data.EM);
+            setIsLoading(false);
         }
     };
 
@@ -64,7 +69,12 @@ function Login(props) {
                 </div>
                 <span className='forgot-password'>Forgot password?</span>
                 <div>
-                    <button className='btn-submit' onClick={() => handleLogin()}>Login</button>
+                    <button className='btn-submit' disabled={isLoading} onClick={() => handleLogin()}>
+                        {isLoading &&
+                            <ImSpinner10 className='loader-icon' />
+                        }
+                        <span>Login</span>
+                    </button>
                 </div>
                 <div className='text-center'>
                     <span className='back' onClick={() => { navigate('/'); }}>&#60;&#60; Go to Homepage</span>
